c60a9a0818bb84ca30df6fc6f8d3d52f4dfb8817
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-12-28 22:46:00 +0900

    add y3c-stl

---
+++ Formula/rang.rb
@@ -0,0 +1,19 @@
+class Rang < Formula
+  desc "A Minimal, Header only Modern c++ library for terminal goodies 💄✨"
+  homepage "https://agauniyal.github.io/rang/"
+  url "https://github.com/agauniyal/rang/archive/refs/tags/v3.2.tar.gz"
+  sha256 "8b42d9c33a6529a6c283a4f4c73c26326561ccc67fbb3e6a3225edd688b39973"
+  license "Unlicense"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
