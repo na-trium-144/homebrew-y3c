cf299bd376d756af597d86a8098859cf3524a4f4
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-12-29 02:01:48 +0900

    fix dependency
    Closes #1.
    
    Signed-off-by: k. Naka <100704180+na-trium-144@users.noreply.github.com>

--- Formula/cpptrace.rb
+++ Formula/cpptrace.rb
@@ -5,12 +5,16 @@ class Cpptrace < Formula
   sha256 "1241790cace5d59ddf21ce5d046f71cd26448a3c8c15d123157498ba81e3543d"
   license "MIT"
 
+  depends_on "ccache" => :build
   depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
   depends_on "dwarfutils"
+  depends_on "zstd"
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-      "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=On", "-DCPPTRACE_FIND_LIBDWARF_WITH_PKGCONFIG=ON"
+      "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=On", "-DCPPTRACE_FIND_LIBDWARF_WITH_PKGCONFIG=ON",
+      "-DBUILD_SHARED_LIBS=ON"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
